a6dc063782a3d3d24c4cc0b3205a7ab17bcc76d8
Aleksandr Gromov <gromov.producer@gmail.com>
2022-04-25 23:05:02 +0300

    Задание №3

---
+++ DZ-3.py
@@ -0,0 +1,6 @@
+# Задание №3
+
+seasons = {1: 'Winter', 2: 'Winter', 12: 'Winter', 3: 'Spring', 4: 'Spring', 5: 'Spring', 6: 'Summer', 7: 'Summer',
+           8: 'Summer', 9: 'Autumn', 10: 'Autumn', 11: 'Autumn'}
+a = int(input('Введите число месяца'))
+print(seasons.get(a))

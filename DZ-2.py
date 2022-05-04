865fc873137f90c69f76efc734b45de6309d5dc2
Aleksandr Gromov <gromov.producer@gmail.com>
2022-05-04 16:06:59 +0300

    Задание №2 update

--- DZ-2.py
+++ DZ-2.py
@@ -1,7 +1,6 @@
 # Задание №2
 
-def user_info(**kwargs):
-    return kwargs
-print(user_info(name = input('Введите имя: '), surname = input('Введите фамилию: '), age = int(input('Введите возраст: ')),
-                birth_year = int(input('Введите год рождения: ')), town = input('Введите город проживания: '),
-                email = input('Введите email: '), phone = input('Введите номер телефона: ')))
+def user_info(name, surname, year, city, email, telephone):
+    return ' '.join([name, surname, year, city, email, telephone])
+print(user_info(surname='Gromov', name='Aleksandr', year='2000', city='Moscow', email='error@mail.ru',
+              telephone='8-800-123-45-67'))
